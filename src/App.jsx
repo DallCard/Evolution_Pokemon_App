0fd2d37bd2ac62fb6fa7d87bda72a142c028b008
Dallana Cardoza Martinez <dallanacardoza16@gmail.com>
2023-06-05 09:39:06 -0600

    se crean las tarjetas de la evolucion y se le da estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,47 @@
+//components
 import { Button } from "./components/Button"
+import { Card } from "./components/Card";
+//Styles
 import './sass/App.scss'
+// Icons
 import { TiArrowLeftOutline } from "react-icons/ti";
 import { TiArrowRightOutline } from "react-icons/ti";
+//Hooks
 import { useState } from "react";
+
 
 
 const App = ()=>{
 
-  const [pokemonId, setPokemonId]= useState(1)
+  const [pokemonId, setPokemonId]= useState(1);
   
-  
+  function prevClick() {
+      (pokemonId === 1)?
+      setPokemonId(1):
+      setPokemonId(pokemonId -1)    
+      }
+
+  function nextClick(){
+    setPokemonId(pokemonId + 1)
+ 
+  }
+ 
     return(
     <>
 
-   { /*tarjetas*/ }
+    <div className="card-container">
+      <Card />
+    </div>
    <div className="buttons-container">
      <Button 
      icon={<TiArrowLeftOutline/>}
-     handleClick={()=>{console.log('anterior')}} 
+     handleClick={prevClick} 
      />
+     {pokemonId}
 
      <Button 
      icon={<TiArrowRightOutline/>}
-     handleClick={()=>{console.log('siguiente')}} 
+     handleClick={nextClick} 
      />
     
     </div>
