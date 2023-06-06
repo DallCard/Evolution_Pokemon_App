f1dba031e8baeb9c98908469e1b07250bce82b74
Dallana Cardoza Martinez <dallanacardoza16@gmail.com>
2023-06-05 22:10:47 -0600

    las tarjetas  muestran la evolucion

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,49 @@ import './sass/App.scss'
 import { TiArrowLeftOutline } from "react-icons/ti";
 import { TiArrowRightOutline } from "react-icons/ti";
 //Hooks
-import { useState } from "react";
-
-
+import { useState, useEffect } from "react";
 
 const App = ()=>{
 
-  const [pokemonId, setPokemonId]= useState(1);
+  const [pokemonId, setPokemonId]= useState(60);
+  const [pokemonEvolutions, setPokemonEvolutions] = useState([])
+
+  useEffect(()=>{
+    getEvolution(pokemonId); 
+     }, [pokemonId])
+  async function getEvolution(id) {
+    const response = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`)
+   // console.log(response)
+    const data = await response.json()
+
+   let pokemonEvoArray = []
+
+   let pokemonLv1 = data.chain.species.name
+   let pokemonLv1Img = await getPokemonImg(pokemonLv1)
+   pokemonEvoArray.push([pokemonLv1, pokemonLv1Img])
+
+   if(data.chain.evolves_to.length !== 0){
+   let pokemonLv2 = data.chain.evolves_to[0].species.name;
+   let pokemonLv2Img = await getPokemonImg(pokemonLv2)
+   pokemonEvoArray.push([pokemonLv2, pokemonLv2Img])
+   //console.log(pokemonEvoArray)
+
+   if(data.chain.evolves_to[0].evolves_to.length !==0){
+    let pokemonLv3 = data.chain.evolves_to[0].evolves_to[0].species.name;
+   let pokemonLv3Img = await getPokemonImg(pokemonLv3)
+   pokemonEvoArray.push([pokemonLv3, pokemonLv3Img])
+   
+  // console.log(pokemonEvoArray)
+   }
+   }
+   setPokemonEvolutions(pokemonEvoArray)
+  }
+
+  async function getPokemonImg(name){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+    const data = await response.json()
+    return data.sprites.other['official-artwork'].front_default
+  }
   
   function prevClick() {
       (pokemonId === 1)?
@@ -27,17 +63,24 @@ const App = ()=>{
   }
  
     return(
-    <>
+    <div className="app">
 
-    <div className="card-container">
-      <Card />
+    <div className={`card-container card${pokemonEvolutions.length}`}>
+      {pokemonEvolutions.map(pokemon => 
+      <Card 
+      key={pokemon[0]}
+      name={pokemon[0]}
+      img={pokemon[1]}
+      />
+      )}
+     
     </div>
    <div className="buttons-container">
      <Button 
      icon={<TiArrowLeftOutline/>}
      handleClick={prevClick} 
      />
-     {pokemonId}
+    
 
      <Button 
      icon={<TiArrowRightOutline/>}
@@ -46,7 +89,7 @@ const App = ()=>{
     
     </div>
 
-   </> )
+   </div> )
 }
 
 export{App}
